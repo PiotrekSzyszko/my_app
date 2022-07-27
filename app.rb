177d570dda29ea66d1749148d79a7dc15f9219b2
Piotrek Szyszko <piotrszyszko86@gmail.com>
2022-07-27 23:22:57 +0200

    Update app.rb
    Wyorzystanie metody dopasowującej r.on

--- app.rb
+++ app.rb
@@ -1,15 +1,11 @@
 require 'roda'
 
-Pizza = Struct.new(:flavor)
-
 class App < Roda
-  plugin :h
-
-  mystery_guest = Pizza.new('Mozzarella')
-
   route do |r|
-    r.get 'mystery_guest' do |_name|
-      "Tajemniczym gosciem jest: #{h mystery_guest}"
+    p 'ROUTE block'
+    r.on 'hello' do
+      p 'HELLO block'
+      'Hello Lucid!'
     end
   end
 end
