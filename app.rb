32d0fd6c106d03ed4db99034d9afa1a7fa685136
Piotrek Szyszko <piotrszyszko86@gmail.com>
2022-07-27 23:00:04 +0200

    Wariant z pluginem :h

--- app.rb
+++ app.rb
@@ -1,9 +1,15 @@
 require 'roda'
 
+Pizza = Struct.new(:flavor)
+
 class App < Roda
+  plugin :h
+
+  mystery_guest = Pizza.new('Mozzarella')
+
   route do |r|
-    r.get 'hello', String do |name|
-      "<h1>Siema #{name} !!</h1>"
+    r.get 'mystery_guest' do |_name|
+      "Tajemniczym gosciem jest: #{h mystery_guest}"
     end
   end
 end
