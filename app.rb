a73272744510e2a0ee6ddd9c8ce89042a01f5113
Piotrek Szyszko <piotrszyszko86@gmail.com>
2022-07-28 07:36:12 +0200

    Update app.rb
    Konsumpcja routów i debugowanie ile dopasowano metodą r.matched_path i ile routów zostało metodą r.remaining_path

--- app.rb
+++ app.rb
@@ -2,15 +2,26 @@ require 'roda'
 
 class App < Roda
   route do |r|
-    p 'ROUTE block'
-    r.on 'hello' do
-      p 'HELLO block'
-      'Hello Lucid!'
-    end
+    p [0, r.matched_path, r.remaining_path]
+    r.on 'posts' do
+      p [1, r.matched_path, r.remaining_path]
+      post_list = {
+        1 => 'Post[1]',
+        2 => 'Post[2]',
+        3 => 'Post[3]',
+        4 => 'Post[4]',
+        5 => 'Post[5]'
+      }
 
-    r.on 'goodbye' do
-      p 'GOODBYE block'
-      'Goodbye Lucid'
+      r.is Integer do |id|
+        p [2, r.matched_path, r.remaining_path]
+        post_list[id]
+      end
+
+      r.is do
+        p [3, r.matched_path, r.remaining_path]
+        post_list.values.map { |post| post }.join(' | ')
+      end
     end
   end
 end
