c64807f7b4aeb43e3145a156237a24e36193c0b8
Piotrek Szyszko <piotrszyszko86@gmail.com>
2022-07-28 10:13:28 +0200

    Update app.rb
    Metoda r.on to rozgałęzienie routa
    a metoda r.in to zakończenie gałęzi routa

--- app.rb
+++ app.rb
@@ -1,10 +1,10 @@
 require 'roda'
 
 class App < Roda
+  # /
   route do |r|
-    p [0, r.matched_path, r.remaining_path]
+    # /posts -> rozgałęzienie
     r.on 'posts' do
-      p [1, r.matched_path, r.remaining_path]
       post_list = {
         1 => 'Post[1]',
         2 => 'Post[2]',
@@ -12,14 +12,23 @@ class App < Roda
         4 => 'Post[4]',
         5 => 'Post[5]'
       }
-
-      r.is Integer do |id|
-        p [2, r.matched_path, r.remaining_path]
-        post_list[id]
+      # /posts/:id -> rozgałęzienie
+      r.on Integer do |id|
+        post = post_list[id]
+        # /posts/:id/show -> rozgałęzienie
+        r.on 'show' do
+          # /posts/:id/show -> koniec gałęzi
+          r.is do
+            "Showing #{post}"
+          end
+          # /posts/:id/show/details -> koniec gałęzi
+          r.is 'detail' do
+            "Showing #{post} | Last access: #{Time.now.strftime('%H:%M:%S')}"
+          end
+        end
       end
-
+      # posts -> koniec gałęzi
       r.is do
-        p [3, r.matched_path, r.remaining_path]
         post_list.values.map { |post| post }.join(' | ')
       end
     end
